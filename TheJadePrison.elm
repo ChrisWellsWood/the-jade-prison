a3b550e969ebd330e984a72a2e20fdfd361d2708
Chris Wells Wood <cwwoodesq@gmail.com>
2017-03-11 12:46:30 +0000

    Added player information section.

--- TheJadePrison.elm
+++ TheJadePrison.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
@@ -16,7 +17,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    (Model 0) ! []
+    (Model "" "" "Dawn" "" "" "Archery") ! []
 
 
 
@@ -24,7 +25,12 @@ init =
 
 
 type alias Model =
-    { attribute : Int
+    { name : String
+    , player : String
+    , caste : String
+    , concept : String
+    , anima : String
+    , supernalAbility : String
     }
 
 
@@ -33,14 +39,34 @@ type alias Model =
 
 
 type Msg
-    = IncrementAttribute
+    = EditName String
+    | EditPlayer String
+    | EditCaste String
+    | EditConcept String
+    | EditAnima String
+    | EditSupernal String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        IncrementAttribute ->
-            { model | attribute = model.attribute + 1 } ! []
+        EditName name ->
+            { model | name = name } ! []
+
+        EditPlayer player ->
+            { model | player = player } ! []
+
+        EditCaste caste ->
+            { model | caste = caste } ! []
+
+        EditConcept concept ->
+            { model | concept = concept } ! []
+
+        EditAnima anima ->
+            { model | anima = anima } ! []
+
+        EditSupernal supernal ->
+            { model | supernalAbility = supernal } ! []
 
 
 
@@ -50,6 +76,71 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ text (toString model.attribute)
-        , button [ onClick IncrementAttribute ] [ text "+" ]
+        [ input [ placeholder "Name", onInput EditName ] []
+        , input [ placeholder "Player", onInput EditPlayer ] []
+        , casteSelect
+        , br [] []
+        , input [ placeholder "Concept", onInput EditConcept ] []
+        , input [ placeholder "Anima", onInput EditAnima ] []
+        , supernalSelect
         ]
+
+
+casteSelect : Html Msg
+casteSelect =
+    select
+        [ onInput EditCaste ]
+        (List.map simpleOption castes)
+
+
+simpleOption : String -> Html msg
+simpleOption val =
+    option [ value val ] [ text val ]
+
+
+castes : List String
+castes =
+    [ "Dawn"
+    , "Zenith"
+    , "Twilight"
+    , "Night"
+    , "Eclipse"
+    ]
+
+
+supernalSelect : Html Msg
+supernalSelect =
+    select
+        [ onInput EditSupernal ]
+        (List.map simpleOption abilities)
+
+
+abilities : List String
+abilities =
+    [ "Archery"
+    , "Athletics"
+    , "Awareness"
+    , "Brawl"
+    , "Bureaucracy"
+    , "Craft"
+    , "Dodge"
+    , "Integrity"
+    , "Investigation"
+    , "Larceny"
+    , "Linguistics"
+    , "Lore"
+    , "Martial Arts"
+    , "Medicine"
+    , "Melee"
+    , "Occult"
+    , "Performance"
+    , "Presence"
+    , "Resistance"
+    , "Ride"
+    , "Sail"
+    , "Socialize"
+    , "Stealth"
+    , "Survival"
+    , "Thrown"
+    , "War"
+    ]
